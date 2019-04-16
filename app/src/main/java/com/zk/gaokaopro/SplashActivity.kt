d2cd187629492d24776b527ac98b53206969c327
WangQing <wangqing11@100tal.com>
2019-04-16 20:08:41 +0800

    1. 添加权限； 2. 修改构建版本； 3. 添加新组件文件。

--- app/src/main/java/com/zk/gaokaopro/SplashActivity.kt
+++ app/src/main/java/com/zk/gaokaopro/SplashActivity.kt
@@ -16,8 +16,8 @@ class SplashActivity : BaseActivity() {
     }
 
     private val permissions = arrayOf(
-//        Manifest.permission.READ_PHONE_STATE,
         Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.READ_PHONE_STATE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     private var splashHandler : Handler = Handler {
